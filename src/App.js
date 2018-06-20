ce5aa64c908bc0968e99793aac5fbe7607768f42
Beliar <dsja93@gmail.com>
2018-06-21 00:23:57 +0200

    move data to service

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import React, { Component } from "react";
-
-const data = [
-  { name: "Arsenal", F: 79, A: 36, Pts: 87 },
-  { name: "Liverpool", F: 67, A: 30, Pts: 80 },
-  { name: "Manchester_U", F: 87, A: 45, Pts: 77 },
-  { name: "Newcastle", F: 74, A: 52, Pts: 71 },
-  { name: "Leeds", F: 53, A: 37, Pts: 66 },
-  { name: "Chelsea", F: 66, A: 38, Pts: 64 },
-  { name: "West_Ham", F: 48, A: 57, Pts: 53 },
-  { name: "Aston_Villa", F: 46, A: 47, Pts: 50 },
-  { name: "Tottenham", F: 49, A: 53, Pts: 50 },
-  { name: "Blackburn", F: 55, A: 51, Pts: 46 },
-  { name: "Southampton", F: 46, A: 54, Pts: 45 },
-  { name: "Middlesbrough", F: 35, A: 47, Pts: 45 },
-  { name: "Fulham", F: 36, A: 44, Pts: 44 },
-  { name: "Charlton", F: 38, A: 49, Pts: 44 },
-  { name: "Everton", F: 45, A: 57, Pts: 43 },
-  { name: "Bolton", F: 44, A: 62, Pts: 40 },
-  { name: "Sunderland", F: 29, A: 51, Pts: 40 },
-  { name: "Ipswich", F: 41, A: 64, Pts: 36 },
-  { name: "Derby", F: 33, A: 63, Pts: 30 },
-  { name: "Leicester", F: 30, A: 64, Pts: 28 }
-];
+import { getResults } from "./services/persistanceService";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {list: []}
+  }
+
+  componentDidMount() {
+    const list = getResults()
+    this.setState({list})
+  }
+
   renderTeams() {
-    return data.map((result, idx) => (
+    return this.state.list.map((result, idx) => (
       <tr key={idx}>
         <td>{result.name}</td>
         <td>{result.F}</td>
